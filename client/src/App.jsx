51c3cfb50ad832f08b07f2cd92c808808371bfc8
DOOM Guy 1485 <theboss1485@gmail.com>
2024-01-23 17:27:51 -0500

    Added ApolloProvider to App.js File

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,28 @@
+
+// I took the code for adding the ApolloProvider component from activity 13 of module 21.
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+})
+
 function App() {
     
     return (
+        
         <>
-            <Navbar />
-            <Outlet />
+            <ApolloProvider client={client}>
+                <Navbar />
+                <Outlet />
+            </ApolloProvider>
         </>
+        
     );
 }
 
