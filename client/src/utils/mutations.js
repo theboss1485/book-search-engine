7a96890f0e5dc057d90b1a4affb676c64b8a1fc8
DOOM Guy 1485 <theboss1485@gmail.com>
2024-01-23 17:17:27 -0500

    Added Front-End mutations.js File

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,48 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+
+    mutation login($email: String!, $password: String!){
+
+        login(email: $email, password: $password) {
+
+            email
+        }
+    }
+`
+
+export const ADD_USER = gql`
+
+    mutation addUser($username: String!, $email: String!, $password: String!){
+
+        addUser(username: $username, email: $email, $password: password){
+            _id
+            username
+            email
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+
+    mutation saveBook($input: saveBookInput!){
+        
+        saveBook(input: $input) {
+
+            id
+            title
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+
+    mutation removeBook($bookId: String!){
+
+        removeBook(bookId: $bookId) {
+            
+            id
+            title
+        }
+    }
+`
